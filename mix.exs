60eb06496c2121ed494ff4409ad9b299b7254d6b
Jesse Herrick <jessegrantherrick@gmail.com>
2016-06-22 12:34:03 -0400

    Use Elixir version 1.3

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Quinn.Mixfile do
   def project do
     [app: :quinn,
      version: "0.0.5",
-     elixir: ">= 1",
+     elixir: "~> 1.3",
      deps: deps,
      description: description,
      package: package]
