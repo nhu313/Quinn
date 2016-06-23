0fc0bcf8f146425c0b5a4e750fcc95826261cd8d
Nhu Nguyen <nhu313@gmail.com>
2016-06-23 10:59:32 -0400

    updated version to use Elixir 1.0

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Quinn.Mixfile do
 
   def project do
     [app: :quinn,
-     version: "0.0.4",
+     version: "0.0.5",
      elixir: "~> 1.0",
      deps: deps,
      description: description,
