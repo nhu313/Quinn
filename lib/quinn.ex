3d1b7d8cb6bcaf3ef62864e9616ae844b3dea9f8
Nhu Nguyen <nhu313@gmail.com>
2017-12-05 09:22:58 -0500

    clean up Quinn.ex to default to a map

--- lib/quinn.ex
+++ lib/quinn.ex
@@ -1,7 +1,7 @@
 defmodule Quinn do
 
-  def parse(xml, strip_namespaces \\ nil) do
-    Quinn.XmlParser.parse(xml, strip_namespaces)
+  def parse(xml, options \\ %{}) do
+    Quinn.XmlParser.parse(xml, options)
   end
 
   def find(node, node_names) do
